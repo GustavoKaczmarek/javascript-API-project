29d9ca266dd8e56c1bf976b4e2761f50d04cfcb1
Gustavo <gustavo_ramirez@outlook.com>
2025-09-13 12:03:28 +0800

    Clase 10 error carga duplicada de datos

--- src/main.js
+++ src/main.js
@@ -12,9 +12,10 @@ async function getTrendingMoviesPreview() {
     const { data } = await api('trending/movie/day');
     const movies = data.results;
 
-    movies.forEach(movie => {
-        const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    trendingMoviesPreviewList.innerHTML = "";
 
+    movies.forEach(movie => {
+        
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
 
@@ -35,9 +36,10 @@ async function getCategoriesPreview() {
     const { data } = await api('genre/movie/list');
     const categories = data.genres;
 
-
+    categoriesPreviewList.innerHTML = ""
+    
     categories.forEach(category => {
-        const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
+        
 
         const categoryContainer = document.createElement('div');
         categoryContainer.classList.add('category-container');
